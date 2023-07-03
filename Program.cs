e30845543f28ea38a889a243adfa3ad0d8c8a23d
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-03 21:30:52 +0300

    diagonal left and right are implemented

--- Program.cs
+++ Program.cs
@@ -8,3 +8,10 @@ arr.Add(list);
 list.Clear();
 list.AddRange(new List<int>() { 9, 8, 9 });
 arr.Add(list);
+
+for (int i = 0, j = arr.Count - 1; i < arr.Count && j >= 0; i++, j--)
+{
+    //Console.WriteLine($"{i} {j} arr[i][j] ");
+    Console.WriteLine($"{i} {i} arr[i][i] ");
+
+}
